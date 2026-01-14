ec6ac41171fe20e30ee4b6a494cda1b87f5a8f22
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2026-01-14 19:53:04 +0530

    update cors

--- src/index.js
+++ src/index.js
@@ -12,10 +12,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors({
-    origin: "https://task-fronted-nu.vercel.app",
+app.use(
+  cors({
+    origin: "https://task-fronted-nu.vercel.app", 
     credentials: true,
-}));
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors());
 
 connectDB()
 import taskRouter from "./router/route.js"
