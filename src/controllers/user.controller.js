b9a4b24f128119f1952e34d0291a92772338b080
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2026-01-14 19:42:00 +0530

    Refactor auth function: simplify login response structure by returning user object directly

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -80,11 +80,7 @@ const auth = async (req, res) => {
       .status(200)
       .json({
         message: "Login successful",
-        user: {
-          id: user._id,
-          name: user.name,
-          email: user.email,
-        },
+        user
       });
     }
 
