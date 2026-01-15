51367c60ea1cae9ac24d04d5b98984e4d7c6ca78
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2026-01-15 10:25:53 +0530

    update

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "https://task-fronted-nu.vercel.app",
+    origin: "https://task-fronted-three.vercel.app",
     credentials: true,
 }));
 
