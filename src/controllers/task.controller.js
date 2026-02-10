fbc5e42b394b336208b0d8383afeff3a46950166
AmarJeetSharma6802 <amarsharma6802@gmail.com>
2026-02-10 11:57:50 +0530

    add task management routes and controller for creating, updating, and deleting tasks

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,70 @@
+import Task from "../model/task.model.js"
+
+
+const CreateTask = async(req,res)=>{
+
+    const {title ,description} = req.body
+
+    if(!title ||!title){
+           return res.status(404).json({ message: "All fields are required" });
+    }
+
+    const task = await Task.create({
+        user: req.user.id, 
+        title,
+        description
+    })
+
+    return res.status(201).json({ message: "Task create successfully",task });
+}
+
+const updateTaks = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
+
+    let updateTaks = { title, description };
+
+    const uptTask = await Task.findOneAndUpdate(
+       id,
+      updateTaks,
+      { new: true }
+    );
+
+    if (!uptTask) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    return res.status(200).json({
+      message: "Task updated successfully",
+      task: uptTask,
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+const deleteTask = async (req, res) => {
+
+     const { id } = req.params;
+
+    const deletedTask = await Task.findOneAndDelete(id);
+
+    if (!deletedTask) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+      return res.status(200).json({
+      message: "Task deleted successfully",
+      task: deletedTask,
+    });
+}
+
+
+export {
+    CreateTask,
+    updateTaks,
+    deleteTask
+}
--- src/router/route.js
+++ src/router/route.js
@@ -1,6 +1,9 @@
 import Router from "express";
 import {auth,logout,fetchData,getData} from "../controllers/user.controller.js"
 import {authUser} from "../middleware/auth.middlware.js"
+
+import {CreateTask ,updateTaks,deleteTask} from "../controllers/task.controller.js"
+
 const router = Router()
 
 
@@ -9,7 +12,11 @@ router.route("/logout").post(authUser,logout)
 router.route("/user").get(authUser,fetchData)
 router.route("/getData").get(getData)
 
+// Task route 
 
+router.route("/createTask").post(authUser ,CreateTask)
+router.route("/updateTaks/:id").put(authUser ,updateTaks)
+router.route("/deleteTask/:id").delete(authUser,deleteTask)
 
 
 export default router
